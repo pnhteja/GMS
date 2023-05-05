c812fb889f40e6ac0983dc036e2dcf9fe7d1efcf
Praneeth-01 <nistalapraneeth@gmail.com>
2023-05-05 18:47:18 +0530

    Updated Server

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,10 @@ const methodOverride = require("method-override");
 // Suppressing the warning
 mongoose.set("strictQuery", true);
 
-/*---------- Connecting to Database ----------*/
 const dbUrl = process.env.DB_URL || "mongodb://127.0.0.1:27017/gms";
 
 mongoose.connect(dbUrl);
 
-// Error Handling
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "Connection Error:"));
 db.once("open", () => {
@@ -22,24 +20,33 @@ db.once("open", () => {
 const app = express();
 const port = 5000;
 
-app.set("views", path.join(__dirname, "views"));
+// app.set("views", path.join(__dirname, "views"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
-/*---------- Requiring Routes ----------*/
 const authRoutes = require("./routes/auth");
 const grievancesRoutes = require("./routes/grievances");
 const grievanceRoutes = require("./routes/grievance");
 const voteRoutes = require("./routes/vote");
 const commentsRoutes = require("./routes/comments");
+const feedbackRoutes = require("./routes/feedback");
+const categoriesRoutes = require("./routes/categories");
+const categoryRoutes = require("./routes/category");
 
 app.use("/auth", authRoutes);
 app.use("/grievances", grievancesRoutes);
-app.use("/grievance/info", grievanceRoutes);
+app.use("/grievance", grievanceRoutes);
 app.use("/grievance/vote", voteRoutes);
 app.use("/grievance/comments", commentsRoutes);
+app.use("/grievance/feedback", feedbackRoutes);
+app.use("/categories", categoriesRoutes);
+app.use("/category", categoryRoutes);
+
+// app.all("*", (req, res) => {
+//   console.log(req.originalUrl);
+// });
 
 app.listen(port, () => {
   console.log(`Serving on port ${port}`);
